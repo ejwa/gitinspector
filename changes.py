cf261728bef976e437dfa861dc0bc2dd73b96421
Adam Waldenberg <adam.waldenberg@ejwa.se>
2012-05-09 09:35:27 +0200

    The changes module now uses the -w flag when calling git.
    This makes the generated statistics more coherent with the output from the
    blame module.

--- changes.py
+++ changes.py
@@ -80,7 +80,7 @@ class AuthorInfo:
 class Changes:
 	def __init__(self, repo, hard):
 		self.commits = []
-		git_log_r = sysrun.run(repo, "git log --pretty='%ad|%t|%aN|%s' --stat=100000 --no-merges --ignore-space-change " +
+		git_log_r = sysrun.run(repo, "git log --pretty='%ad|%t|%aN|%s' --stat=100000 --no-merges -w " +
 		                       "-C {0} --date=short".format("-C" if hard else ""))
 		commit = None
 		found_valid_extension = False
