128b8b3b5603c219449c1111b1df6e67550569ea
Adam Waldenberg <adam.waldenberg@ejwa.se>
2013-05-20 02:20:29 +0200

    The responsibilities module no longer prints empty authors.

--- responsibilities.py
+++ responsibilities.py
@@ -52,32 +52,34 @@ class ResponsibilitiesOutput(Outputable):
 		print("\n" + textwrap.fill(__responsibilities_info_text__ + ":", width=terminal.get_size()[0]))
 
 		for i in sorted(set(i[0] for i in blame.get(self.hard).blames)):
-			print("\n" + i, "is mostly responsible for:")
 			responsibilities = sorted(((i[1], i[0]) for i in Responsibilities.get(self.hard, i)), reverse=True)
+			if responsibilities:
+				print("\n" + i, "is mostly responsible for:")
 
-			for j, entry in enumerate(responsibilities):
-				(width, _) = terminal.get_size()
-				width -= 7
+				for j, entry in enumerate(responsibilities):
+					(width, _) = terminal.get_size()
+					width -= 7
 
-				print(str(entry[0]).rjust(6), end=" ")
-				print("...%s" % entry[1][-width+3:] if len(entry[1]) > width else entry[1])
+					print(str(entry[0]).rjust(6), end=" ")
+					print("...%s" % entry[1][-width+3:] if len(entry[1]) > width else entry[1])
 
-				if j >= 9:
-					break
+					if j >= 9:
+						break
 
 	def output_html(self):
 		resp_xml = "<div><div class=\"box\">"
 		resp_xml += "<p>" + __responsibilities_info_text__ + ".</p>"
 
 		for i in sorted(set(i[0] for i in blame.get(self.hard).blames)):
-			resp_xml += "<h3>" + i + "is mostly responsible for</h3>"
 			responsibilities = sorted(((i[1], i[0]) for i in Responsibilities.get(self.hard, i)), reverse=True)
+			if responsibilities:
+				resp_xml += "<h3>" + i + "is mostly responsible for</h3>"
 
-			for j, entry in enumerate(responsibilities):
-				resp_xml += "<p>" + entry[1] + " (" + str(entry[0]) + " eloc)</p>"
+				for j, entry in enumerate(responsibilities):
+					resp_xml += "<p>" + entry[1] + " (" + str(entry[0]) + " eloc)</p>"
 
-				if j >= 9:
-					break
+					if j >= 9:
+						break
 
 		resp_xml += "</div></div>"
 		print(resp_xml)
@@ -87,21 +89,22 @@ class ResponsibilitiesOutput(Outputable):
 		resp_xml = ""
 
 		for i in sorted(set(i[0] for i in blame.get(self.hard).blames)):
-			resp_xml += "\t\t\t<author>\n"
-			resp_xml += "\t\t\t\t<name>" + i + "</name>\n"
-			resp_xml += "\t\t\t\t<files>\n"
 			responsibilities = sorted(((i[1], i[0]) for i in Responsibilities.get(self.hard, i)), reverse=True)
+			if responsibilities:
+				resp_xml += "\t\t\t<author>\n"
+				resp_xml += "\t\t\t\t<name>" + i + "</name>\n"
+				resp_xml += "\t\t\t\t<files>\n"
 
-			for j, entry in enumerate(responsibilities):
-				resp_xml += "\t\t\t\t\t<file>\n"
-				resp_xml += "\t\t\t\t\t\t<name>" + entry[1] + "</name>\n"
-				resp_xml += "\t\t\t\t\t\t<rows>" + str(entry[0]) + "</rows>\n"
-				resp_xml += "\t\t\t\t\t</file>\n"
+				for j, entry in enumerate(responsibilities):
+					resp_xml += "\t\t\t\t\t<file>\n"
+					resp_xml += "\t\t\t\t\t\t<name>" + entry[1] + "</name>\n"
+					resp_xml += "\t\t\t\t\t\t<rows>" + str(entry[0]) + "</rows>\n"
+					resp_xml += "\t\t\t\t\t</file>\n"
 
-				if j >= 9:
-					break
+					if j >= 9:
+						break
 
-			resp_xml += "\t\t\t\t</files>\n"
-			resp_xml += "\t\t\t</author>\n"
+				resp_xml += "\t\t\t\t</files>\n"
+				resp_xml += "\t\t\t</author>\n"
 
 		print("\t<responsibilities>\n" + message_xml + "\t\t<authors>\n" + resp_xml + "\t\t</authors>\n\t</responsibilities>")
