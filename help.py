466941047b77f71879f21e4a21de62d944b52411
Adam Waldenberg <adam.waldenberg@ejwa.se>
2013-06-12 18:15:14 +0200

    Slightly improved the documentation of the --until/since flags.

--- help.py
+++ help.py
@@ -42,10 +42,10 @@ Mandatory arguments to long options are mandatory for short options too.
                                  analysis of commits
   -r  --responsibilities       show which files the different authors seem
                                  most responsible for
-      --since=DATE             show statistics for commits more recent than a
-                                 specific date
+      --since=DATE             only show statistics for commits more recent
+                                 than a specific date
   -T, --timeline               show commit timeline, including author names
-      --until=DATE             show  statistics for commits older than a
+      --until=DATE             only show statistics for commits older than a
                                  specific date
   -w                           show all statistical information in weeks
                                  instead of in months
