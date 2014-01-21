d8bdbe9f6f93eb25a8ef454941eeb21f406eb722
Adam Waldenberg <adam.waldenberg@ejwa.se>
2014-01-22 00:24:22 +0100

    Fixed broken header in optval module.

--- gitinspector/optval.py
+++ gitinspector/optval.py
@@ -15,6 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
+# along with gitinspector. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 import getopt
