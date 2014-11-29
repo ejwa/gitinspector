26e2ce7c07c8527934a78bb4baceef9843587635
Adam Waldenberg <adam.waldenberg@ejwa.se>
2014-11-29 06:35:07 +0100

    Added help text (--help) for the new revision filtering.

--- gitinspector/help.py
+++ gitinspector/help.py
@@ -61,9 +61,9 @@ Boolean arguments can only be given to long options.
   -w, --weeks[=BOOL]             show all statistical information in weeks
                                    instead of in months
   -x, --exclude=PATTERN          an exclusion pattern describing the file
-                                   paths, author names or author emails that
-                                   should be excluded from the statistics; can
-                                   be specified multiple times
+                                   paths, revisions, author names or author
+                                   emails that should be excluded from the
+                                   statistics; can be specified multiple times
   -h, --help                     display this help and exit
       --version                  output version information and exit
 
